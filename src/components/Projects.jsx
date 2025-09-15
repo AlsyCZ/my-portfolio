2f2b820124561bc2ae6d1224504db85074ed0f1e
Aleš Mrázek <100871677+AlsyCZ@users.noreply.github.com>
2025-09-15 18:18:42 +0200

    Image changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,3 @@
-import router from '../assets/router.png'
-import pc from '../assets/pc.png'
-import work from '../assets/work.png'
 export default function Projects() {
   return (
     <section id="projects" className="bg-gray-100 pt-4 pb-12 px-4 w-full">
@@ -31,19 +28,19 @@ export default function Projects() {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
         {/* Zkušenost 1 */}
         <div className="bg-white shadow-lg rounded-2xl overflow-hidden flex flex-col items-center p-6">
-          <img src={router} alt="Mikrotik" className="w-34 h-34 object-contain mb-4" />
+          <img src="/router.jpg" alt="Mikrotik" className="w-34 h-34 object-contain mb-4" />
           <h3 className="text-xl font-semibold mb-2 text-center">Správa a konfigurace sítí</h3>
           <p className="text-gray-600 text-center">V rámci projektu s Mikrotik routery jsem se více sblížil se síťařinou. Vylepšil jsem si doma internet pomocí Powerline. Zjistil co je to AP, DHCP, jak nastavit VPN. Nastavil jsem si také svojí NASku.</p>
         </div>
         {/* Zkušenost 2 */}
         <div className="bg-white shadow-lg rounded-2xl overflow-hidden flex flex-col items-center p-6">
-          <img src={pc} alt="Hardware" className="w-34 h-34 object-contain mb-4" />
+          <img src="/pc.jpg" alt="Hardware" className="w-34 h-34 object-contain mb-4" />
           <h3 className="text-xl font-semibold mb-2 text-center">Práce s Hardwarem</h3>
           <p className="text-gray-600 text-center">Odjakživa mám rád počítačový hardware. Opravil jsem i postavil několik počítačů. Pomáhal vybírat komponenty do sestav. Vyčistil několik notebooků. A zpousty dalšího.</p>
         </div>
         {/* Zkušenost 3 */}
         <div className="bg-white shadow-lg rounded-2xl overflow-hidden flex flex-col items-center p-6">
-          <img src={work} alt="Mobilní elektronika" className="w-34 h-34 object-contain mb-4" />
+          <img src="/work.jpg" alt="Mobilní elektronika" className="w-34 h-34 object-contain mb-4" />
           <h3 className="text-xl font-semibold mb-2 text-center">Servis mobilní elektroniky</h3>
           <p className="text-gray-600 text-center">Rád také pracuji s mobilními telefony. Opravil jsem již několik telefonů (iPhony i Androidy) a několik jich také prodal pro zpětný výdělek.</p>
         </div>
